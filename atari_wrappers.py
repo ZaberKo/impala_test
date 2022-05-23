a062b39f9c58962704491d3509e1b0b47abde03f
zaber <20830726+ZaberKo@users.noreply.github.com>
2022-05-23 22:17:13 +0800

    wip

--- atari_wrappers.py
+++ atari_wrappers.py
@@ -4,14 +4,16 @@ from ray.rllib.env.wrappers.atari_wrappers import get_wrapper_by_cls, FrameStack
 from ray.tune.registry import register_env
 from ray.rllib.env.wrappers.atari_wrappers import MonitorEnv
 
-def register_my_env(env_name, max_episode_steps=3600*5):
+
+def register_my_env(env_name, max_episode_steps: int = None):
 
     def env_creator(env_config):
         env = gym.make(env_name, **env_config)
-        timelimit_wrapper = get_wrapper_by_cls(env, TimeLimit)
+        if max_episode_steps is not None:
+            timelimit_wrapper = get_wrapper_by_cls(env, TimeLimit)
 
-        env = TimeLimit(timelimit_wrapper.env,
-                        max_episode_steps=max_episode_steps)
+            env = TimeLimit(timelimit_wrapper.env,
+                            max_episode_steps=max_episode_steps)
 
         env = MyMonitorEnv(env)
         env = AtariPreprocessing(env,
@@ -27,16 +29,17 @@ def register_my_env(env_name, max_episode_steps=3600*5):
         env = FrameStack(env, 4)
         return env
 
-    new_env_name=f"{env_name}-TimeLimit{max_episode_steps}"
-    register_env(new_env_name,env_creator)
+    new_env_name = f"{env_name}-TimeLimit{max_episode_steps}"
+    register_env(new_env_name, env_creator)
 
     return new_env_name
+
 
 class MyMonitorEnv(MonitorEnv):
     def __init__(self, env=None):
         super().__init__(env)
-        self._total_steps=0
-        self._done=True
+        self._total_steps = 0
+        self._done = True
 
     def reset(self, **kwargs):
         obs = self.env.reset(**kwargs)
@@ -49,8 +52,7 @@ class MyMonitorEnv(MonitorEnv):
             self._episode_rewards.append(self._current_reward)
             self._episode_lengths.append(self._num_steps)
             self._num_episodes += 1
-            self._done=True
-
+            self._done = True
 
         return obs
 
@@ -60,7 +62,7 @@ class MyMonitorEnv(MonitorEnv):
         self._num_steps += 1
         self._total_steps += 1
 
-        self._done=done
+        self._done = done
 
         if done:
             self._episode_rewards.append(self._current_reward)
